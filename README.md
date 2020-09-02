9f8659b73c3e48356967914d97a63535d5fd151d
Sunday Ishaya <imizallah@gmail.com>
2020-09-02 10:10:56 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Video calls app built with Nodejs
+### Ishaya Sunday 
+# Email: ishayasunday@gmail.com
+
+
+### Technologies used
+# 1. Web Sockets
+# 2. Socket.IO
+# 3. Nodejs
+# 4. Express
+# 5. Bootstrap 4
+# 6. FontAwesome
+
+# Please email me if you have any errors or any questions
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+// Uncomment to properly setup cors on your application
+// const cors = require('cors')
+// app.use(cors())
+const server = require('http').Server(app);
+const logger = require('morgan');
+const io = require('socket.io')(server);
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
+const { v4: uuidV4 } = require('uuid')
+
+app.use('/peerjs', peerServer);
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+    // messages
+    socket.on('message', (message) => {
+      //send message to the same room
+      io.to(roomId).emit('createMessage', message)
+  }); 
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
+
+const port = process.env.PORT||3030;
+
+// Setting the listening area of the server on development
+server.listen(port, () => console.log(`Server running on ${port}`));
